4c16fa753943e65c29ee608283aefed5bb1af974
Josiah Nunemaker <josiah.nunemaker@gmail.com>
2025-08-16 13:32:33 -0400

    Add support for images on ideas

--- src/content.config.ts
+++ src/content.config.ts
@@ -4,11 +4,13 @@ import { glob } from "astro/loaders";
 
 const ideas = defineCollection({
   loader: glob({ pattern: "**/*.md", base: "./src/content/ideas" }),
-  schema: z.object({
-    title: z.string().min(2),
-    description: z.string(),
-    addedDate: z.date(),
-  }),
+  schema: ({ image }) =>
+    z.object({
+      title: z.string().min(2),
+      description: z.string(),
+      addedDate: z.date(),
+      image: image().optional(),
+    }),
 });
 
 export const collections = {
--- src/content/ideas/potluck-bot.md
+++ src/content/ideas/potluck-bot.md
@@ -2,6 +2,7 @@
 title: Potluck Bot
 description: A Discord bot to help organize potlucks by managing food contributions and preferences.
 addedDate: 2025-07-31
+image: ../../assets/potluck-bot.png
 ---
 
 Hey look, I'm some more content!
